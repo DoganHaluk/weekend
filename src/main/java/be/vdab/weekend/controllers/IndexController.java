432e6a479d8b2845ccc17742b8f90377e0227619
DoganHaluk <ha_look@yahoo.com>
2021-11-15 09:12:30 +0100

    first commit

--- src/main/java/be/vdab/weekend/controllers/IndexController.java
+++ src/main/java/be/vdab/weekend/controllers/IndexController.java
@@ -14,11 +14,6 @@ class IndexController {
     @GetMapping
     public ModelAndView index() {
         var dag = LocalDate.now().getDayOfWeek();
-        if (dag == DayOfWeek.SATURDAY || dag == DayOfWeek.SUNDAY) {
-            return new ModelAndView("index", "dag", "weekend!");
-        } else {
-            return new ModelAndView("index", "dag", "werkdag!");
-        }
-
+        return new ModelAndView("index", "weekend", dag == DayOfWeek.SATURDAY || dag == DayOfWeek.SUNDAY);
     }
 }
